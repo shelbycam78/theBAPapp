be8b62adceaf7d44a485abb090c9f08861ea80c1
Shelby Simpson <shelbycam78@gmail.com>
2021-10-28 19:35:31 -0400

    Add project files.

---
+++ theBAPapp.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="theBAPapp.WebMVC.MvcApplication" Language="C#" %>
---
+++ theBAPapp.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(theBAPapp.WebMVC.Startup))]
+namespace theBAPapp.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
